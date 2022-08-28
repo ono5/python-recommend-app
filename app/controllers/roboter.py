c97f2f8f31553436647ffd8eae712aa78a466aa0
ono5 <kuroneko_head_lord@yahoo.co.jp>
2022-08-28 09:51:40 +0900

    connect to template,controller,model

--- app/controllers/roboter.py
+++ app/controllers/roboter.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, render_template, request, url_for
 
 from app.controllers.forms import RateForm
+from app.models.user import User
+from app.models.restaurant import Restaurant
+from app.models.rate import Rate
 import settings
 
 app = Flask(__name__, template_folder=settings.TEMPLATE_FOLDER,
@@ -21,6 +24,8 @@ ROBOT_NAME = 'Roboko'
 def hello() -> str:
     if request.method == "POST":
         user_name = request.form.get("user_name").strip()
+        user = User.get_or_create(user_name)
+
         form = RateForm(request.form)
         form.user_name.data = user_name
         return render_template("evaluate_restaurant.html", user_name=user_name, form=form)
--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,28 @@
 from sqlalchemy import Column, String, UniqueConstraint
 
-from app.models.db import BaseDatabase
+from app.models.db import BaseDatabase, database
 
 
 class User(BaseDatabase):
     __tablename__ = 'user'
     name = Column(String)
     UniqueConstraint(name)
+
+    @staticmethod
+    def get_or_create(name):
+        session = database.connect_db()
+        row = session.query(User).filter(User.name == name).first()
+        if row:
+            session.close()
+            # return exsiting data
+            return row
+
+        user = User(name=name)
+        session.add(user)
+        session.commit()
+        session.close()
+
+        # return user data including ID
+        row = session.query(User).filter(User.name == name).first()
+        session.close()
+        return row
